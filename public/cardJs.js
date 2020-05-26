cbd1456eb11f619055529bf4ca6ee2a4966f0e5f
Hayden Mirza <haydenjwmirza@gmail.com>
2020-05-26 19:40:49 -0400

    Made card loading work properly

--- public/cardJs.js
+++ public/cardJs.js
@@ -14,58 +14,28 @@ function shuffle(array) {
 function loadImages(){
     let imgArr = [];
 
-    //Loading images, will be replaced with server side POST request
-    imgArr.push(new Image(100,100));
-    imgArr.push(new Image(100,100));
-    imgArr[0].src = 'images/feelsSpecialMan.png';
-    imgArr[1].src = 'images/feelsSpecialMan.png';
-    
-    imgArr.push(new Image(100,100));
-    imgArr.push(new Image(100,100));
-    imgArr[2].src = 'images/feelsStrongMan.png';
-    imgArr[3].src = 'images/feelsStrongMan.png';
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            let res = JSON.parse(xhttp.response);
+            for(let x=0;x<res.length;x++){
+                let newimg = new Image(100,100);
+                newimg.src = 'images/' + res[x];
+                imgArr.push(newimg,newimg);
+            }
+            console.log(imgArr.length);
+            imgArr = shuffle(imgArr);
 
-    imgArr.push(new Image(100,100));
-    imgArr.push(new Image(100,100));
-    imgArr[4].src = 'images/HYPERS.png';
-    imgArr[5].src = 'images/HYPERS.png';
-
-    imgArr.push(new Image(100,100));
-    imgArr.push(new Image(100,100));
-    imgArr[6].src = 'images/peepoHappyLove.png';
-    imgArr[7].src = 'images/peepoHappyLove.png';
-    
-    imgArr.push(new Image(100,100));
-    imgArr.push(new Image(100,100));
-    imgArr[8].src = 'images/peepoRip.png';
-    imgArr[9].src = 'images/peepoRip.png';
-    
-    imgArr.push(new Image(100,100));
-    imgArr.push(new Image(100,100));
-    imgArr[10].src = 'images/pepeHands.png';
-    imgArr[11].src = 'images/pepeHands.png';
-    
-    imgArr.push(new Image(100,100));
-    imgArr.push(new Image(100,100));
-    imgArr[12].src = 'images/peepoHug.png';
-    imgArr[13].src = 'images/peepoHug.png';
-    
-    imgArr.push(new Image(100,100));
-    imgArr.push(new Image(100,100));
-    imgArr[14].src = 'images/pepeLaugh.png';
-    imgArr[15].src = 'images/pepeLaugh.png';
-    
-    //Randomize
-    imgArr = shuffle(imgArr);
-
-    for(let x=0;x<imgArr.length;x++){
-        let div = document.getElementById("gridDiv" + (x+1));
-        let img = document.createElement("IMG");
-        img.src = imgArr[x].src;
-        //img.style.visibility = 'hidden';
-        div.appendChild(img);
-    }
-
-    
+            for(let x=0;x<imgArr.length;x++){
+                let div = document.getElementById("gridDiv" + (x+1));
+                let img = document.createElement("IMG");
+                img.src = imgArr[x].src;
+                //img.style.visibility = 'hidden';
+                div.appendChild(img);
+            }
+        }
+    };
+    xhttp.open("POST", "/getImageNames", true);
+    xhttp.send();    
 };
 
